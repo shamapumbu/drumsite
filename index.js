53edad62b402ad102b04e67a750e40da6979ecf0
shamapumbu <88844339+shamapumbu@users.noreply.github.com>
2022-01-20 08:27:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var buttonList = document.querySelectorAll(".drum");
 
 for(var i = 0; i < buttonList.length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        alert("I got clicked!");
+       
+       this.style.color="white";
+       
+        // var audio = new Audio('sounds/tom-1.mp3');
+        // audio.play();
     })
 }
